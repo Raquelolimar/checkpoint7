4a6ff3738f6c4f250f7ae5282c483d56d50a5102
Raquelolimar <raquelolivenza@gmail.com>
2025-06-16 16:44:49 +0200

    first commit

--- ejerc.js
+++ ejerc.js
@@ -1,13 +1,9 @@
-
-function operacion(num1, num2 , num3, num4) {
-    const uno= num1 + num2;
-    const dos= num3 + num4;
-    const tres = uno * dos;
-        if (tres > 50 ) {
-            console.log("El numero es mayor que 50");
-               } else {
-               console.log("El numero es menor que 50"); 
-        }
+function operacion (num1, num2, num3, num4) {
+    const valor = (num1 + num2) * (num3 + num4);
+        if (valor >50) {
+                  console.log("El numero es mayor que 50");
+                       } else {
+                       console.log("El numero es menor que 50"); 
+                    }
 }
-
 operacion(2, 5, 7, 3);
